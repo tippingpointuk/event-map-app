e1927b1e6c4f524bb9c922a0cf78cd952f7fdcee
joe-irving <joeirving.jbi@gmail.com>
2022-01-07 05:05:47 +0000

    Add cambo directory

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/event-map-app/cambo'
+    : '/'
+  ,
+  outputDir: "cambo",
   css: {
     loaderOptions: {
       sass: {
